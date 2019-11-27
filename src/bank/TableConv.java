ebbc3cadfcf5d386295b5ee8aeceb870810bda27
manu <testmanu.test>
2019-11-27 14:31:31 +0100

    Avec la table de conv

--- src/bank/TableConv.java
+++ src/bank/TableConv.java
@@ -7,28 +7,89 @@ import java.util.Hashtable;
 public class TableConv {
 
 	private String numCptInput;
+	Hashtable<String, String> t;	
 	
-	
-	/*
+
 	public TableConv(String numCptInput) {
-		// TODO Auto-generated constructor stub
+
 		this.numCptInput = numCptInput;
-		Hashtable<String, Integer> dic=new Hashtable<String, Integer>();
-		//dic..add["A"] = 1;
-		
+		this.initTable();
 		
 		
 	}
-	*/
 
-	public TableConv(String numCptInput2) {
-		// TODO Auto-generated constructor stub
+	private static boolean isInterger(String number)
+	{
+		try {
+			Integer.parseInt(number);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
+		
 	}
-
+	
 	public BigInteger  conv()
 	{
-		BigInteger result = BigInteger.ZERO;
+		char[] s = this.numCptInput.toCharArray();
 		
-		return result;
+		String result ="" ;
+		
+		
+		for (int i =0; i<=  this.numCptInput.length() ; i++ ) {
+		/*
+			String v =  
+			//if( ) 
+				result += this.t.get(s[i]);
+			else
+				result +=s[i];
+				*/ 
+		}
+		
+		return new BigInteger(result);
+	}
+	
+	private void initTable()
+	{
+		
+		t =new Hashtable<String, String>();
+		t.put("A", "1");
+		t.put("J", "1");
+		
+		t.put("B", "2");
+		t.put("K", "2");
+		t.put("S", "2");
+		
+		t.put("C", "3");
+		t.put("L", "3");
+		t.put("T", "3");
+		
+		t.put("D", "4");
+		t.put("M", "4");
+		t.put("U", "4");
+		
+		t.put("E", "5");
+		t.put("N", "5");
+		t.put("V", "5");
+		
+		t.put("F", "6");
+		t.put("O", "6");
+		t.put("W", "6");
+		
+		
+		t.put("G", "7");
+		t.put("P", "7");
+		t.put("W", "7");
+
+		t.put("H", "8");
+		t.put("Q", "8");
+		t.put("Y", "8");
+		
+		
+		t.put("I", "9");
+		t.put("R", "9");
+		t.put("Z", "9");
+		
+		
 	}
 }
---
+++ src/bank/test/TableConvTest.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package bank.test;
+
+import static org.junit.Assert.*;
+
+import java.math.BigInteger;
+
+import org.junit.Test;
+
+import bank.ICheckCompte;
+import bank.NumCompte;
+import bank.TableConv;
+
+
+
+/**
+ * @author IB
+ *
+ */
+public class TableConvTest {
+
+	@Test()
+	public void testCheckOK() {
+		
+		String numCpt ="123456789AB";		
+		TableConv  t = new TableConv(numCpt);
+		BigInteger resultOk = new BigInteger( "12345678912");
+		assertEquals(resultOk, t.conv());	
+	}
+
+	@Test()
+	public void testCheckKO() {
+		
+		String numCpt ="123456789AB";		
+		TableConv  t = new TableConv(numCpt);
+		BigInteger resultOk = new BigInteger( "12345678913");
+		assertNotEquals(resultOk, t.conv());	
+	}
+	
+}
