0239c3fdfddafdb06ee9192231c87be81df349dc
fabien <IB@WINIB-UK1AC31L9.com>
2019-11-27 12:22:49 +0100

    Classes NumEtablissement et NumGuichet

--- src/bank/NumEtablissement.java
+++ src/bank/NumEtablissement.java
@@ -1,5 +1,26 @@
 package bank;
 
-public class NumEtablissement {
+
+public class NumEtablissement implements ICheckCompte {
+
+	private String numEtab;
+	
+	@Override
+	public boolean check() {
+		
+		boolean test=true;
+		int i=0;
+		// Test caractères ne sont que des chiffres
+		while ( (i<this.numEtab.length() ) && (test==true) )
+		{
+			if (this.numEtab.substring(i).matches("[0-9]"))	{test=false;}
+			i++;
+		}
+		// Test longueur 5
+		if (this.numEtab.length() != 5 ) { test = false;}
+		
+		return test;
+	}
+	
 
 }
--- src/bank/NumGuichet.java
+++ src/bank/NumGuichet.java
@@ -1,6 +1,25 @@
 package bank;
 
-public class NumGuichet {
+public class NumGuichet implements ICheckCompte{
+
+	private String numGuichet;
+	
+	@Override
+	public boolean check() {
+		
+		boolean test=true;
+		int i=0;
+		// Test caractères ne sont que des chiffres
+		while ( (i<this.numGuichet.length() ) && (test==true) )
+		{
+			if (this.numGuichet.substring(i).matches("[0-9]"))	{test=false;}
+			i++;
+		}
+		// Test longueur 5
+		if (this.numGuichet.length() != 5 ) { test = false;}
+		
+		return test;
+	}
 
 	
 
